b072c70d4221b5a9db3e2a20acdcc4be098ba5d2
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-09-14 21:22:33 -0600

    Make the remnants colored

--- prototype/belt-colours.lua
+++ prototype/belt-colours.lua
@@ -26,7 +26,6 @@ local function belt_animation_set(colour)
 		}
 	}--[[@as data.TransportBeltAnimationSetWithCorners]]
 end
-
 ---@param colour PM.belt_colours
 ---@return data.UndergroundBeltStructure
 local function underground_structure(colour)
@@ -146,7 +145,6 @@ local function underground_structure(colour)
 		-- }
 	}--[[@as data.UndergroundBeltStructure]]
 end
-
 ---@param colour PM.belt_colours
 ---@return data.Animation4Way
 function splitter_structure(colour)
@@ -237,7 +235,6 @@ function splitter_structure(colour)
 		}
 	}--[[@as data.Animation4Way]]
 end
-
 ---@param colour PM.belt_colours
 ---@return data.Animation4Way
 function splitter_patch(colour)
@@ -291,8 +288,9 @@ end
 
 --MARK: Icon manipulation
 
+---@alias belt_type "transport"|"splitter"|"underground"
 ---@param pictures data.Sprite[]|data.SpriteSheet|data.Sprite
----@param type "transport"|"splitter"|"underground"
+---@param type belt_type
 ---@param colour PM.belt_colours
 function set_variations(pictures, type, colour)
 	--- Handle every variations in an array of sprites
@@ -319,9 +317,8 @@ function set_variations(pictures, type, colour)
 	pictures.scale = 0.25
 	pictures.mipmap_count = 4
 end
-
 ---@param item data.ItemPrototype|data.EntityPrototype
----@param type "transport"|"splitter"|"underground"
+---@param type belt_type
 ---@param colour PM.belt_colours
 function set_icon(item, type, colour)
 	if not item.icons then
@@ -354,6 +351,8 @@ local undergrounds = data.raw["underground-belt"]
 local splitters = data.raw["splitter"]
 local items = data.raw["item"]
 
+--MARK: Belt modification
+
 ---@param beltTier any
 ---@param colour PM.belt_colours
 local function handle_tier(beltTier, colour)
@@ -373,6 +372,10 @@ local function handle_tier(beltTier, colour)
 	splitter.structure = splitter_structure(colour)
 	splitter.structure_patch = splitter_patch(colour)
 
+	belt.corpse = "pm-"..colour.."-transport-remnant"
+	underground.corpse = "pm-"..colour.."-underground-remnant"
+	splitter.corpse = "pm-"..colour.."-splitter-remnant"
+
 	set_icon(belt, "transport", colour)
 	set_icon(underground, "underground", colour)
 	set_icon(splitter, "splitter", colour)
@@ -382,9 +385,159 @@ local function handle_tier(beltTier, colour)
 	set_icon(splitter_item, "splitter", colour)
 end
 
+---@type table<PM.belt_colours,true>
+local coloured = {}
+
 for tier, beltTier in pairs(PM.belts) do
 	local colour = settings.startup["pm-belt-colour-tier-"..tier]
 	if colour then
+		coloured[colour.value--[[@as PM.belt_colours]]] = true
 		handle_tier(beltTier, colour.value--[[@as PM.belt_colours]])
 	end
+end
+
+---MARK: Remnants
+
+---@param colour PM.belt_colours
+---@param order {[1]:string,[2]:string,[3]:string}
+---@return data.CorpsePrototype[]
+local function make_remnants(colour, order)
+	return {
+		{
+			type = "corpse",
+			name = "pm-"..colour.."-transport-remnant",
+			order = order[1],
+			icon = "__periodic-madness__/graphics/belt-colours/"..colour.."/transport/icon.png",
+			icon_size = 64, icon_mipmaps = 4,
+			flags = {"placeable-neutral", "not-on-map"},
+			subgroup = "belt-remnants",
+			selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+			tile_width = 1,
+			tile_height = 1,
+			selectable_in_game = false,
+			time_before_removed = 60 * 60 * 15, -- 15 minutes
+			final_render_layer = "remnants",
+			animation = make_rotated_animation_variations_from_sheet(2,{
+				filename = "__periodic-madness__/graphics/belt-colours/"..colour.."/transport/remnants/remnants.png",
+				line_length = 1,
+				width = 54,
+				height = 52,
+				frame_count = 1,
+				variation_count = 1,
+				axially_symmetrical = false,
+				direction_count = 4,
+				shift = util.by_pixel(1, 0),
+				hr_version =
+				{
+					filename = "__periodic-madness__/graphics/belt-colours/"..colour.."/transport/remnants/hr-remnants.png",
+					line_length = 1,
+					width = 106,
+					height = 102,
+					frame_count = 1,
+					variation_count = 1,
+					axially_symmetrical = false,
+					direction_count = 4,
+					shift = util.by_pixel(1, -0.5),
+					scale = 0.5
+				}
+			}--[[@as data.SpriteSheet]]),
+		},
+		{
+			type = "corpse",
+			name = "pm-"..colour.."-underground-remnant",
+			order=order[2],
+			icon = "__periodic-madness__/graphics/belt-colours/"..colour.."/underground/icon.png",
+			icon_size = 64, icon_mipmaps = 4,
+			flags = {"placeable-neutral", "not-on-map", "building-direction-8-way"},
+			subgroup = "belt-remnants",
+			selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+			tile_width = 1,
+			tile_height = 1,
+			selectable_in_game = false,
+			time_before_removed = 60 * 60 * 15, -- 15 minutes
+			final_render_layer = "remnants",
+			remove_on_tile_placement = false,
+			animation =
+			{
+				filename = "__periodic-madness__/graphics/belt-colours/"..colour.."/underground/remnants/remnants.png",
+				line_length = 1,
+				width = 78,
+				height =72,
+				frame_count = 1,
+				variation_count = 1,
+				axially_symmetrical = false,
+				direction_count = 8,
+				shift = util.by_pixel(10, 3),
+				hr_version =
+				{
+					filename = "__periodic-madness__/graphics/belt-colours/"..colour.."/underground/remnants/hr-remnants.png",
+					line_length = 1,
+					width = 156,
+					height = 144,
+					frame_count = 1,
+					variation_count = 1,
+					axially_symmetrical = false,
+					direction_count = 8,
+					shift = util.by_pixel(10.5, 3),
+					scale = 0.5
+				}
+			}
+		},
+		{
+			type = "corpse",
+			name = "pm-"..colour.."-splitter-remnant",
+			icon = "__periodic-madness__/graphics/belt-colours/"..colour.."/splitter/icon.png",
+			icon_size = 64, icon_mipmaps = 4,
+			flags = {"placeable-neutral", "not-on-map"},
+			subgroup = "belt-remnants",
+			order = "a-g-a",
+			selection_box = {{-0.9, -0.5}, {0.9, 0.5}},
+			tile_width = 2,
+			tile_height = 1,
+			selectable_in_game = false,
+			time_before_removed = 60 * 60 * 15, -- 15 minutes
+			final_render_layer = "remnants",
+			remove_on_tile_placement = false,
+			animation =
+			{
+				filename = "__periodic-madness__/graphics/belt-colours/"..colour.."/splitter/remnants/remnants.png",
+				line_length = 1,
+				width = 96,
+				height = 96,
+				frame_count = 1,
+				variation_count = 1,
+				axially_symmetrical = false,
+				direction_count = 4,
+				shift = util.by_pixel(4, 1.5),
+				hr_version =
+				{
+					filename = "__periodic-madness__/graphics/belt-colours/"..colour.."/splitter/remnants/hr-remnants.png",
+					line_length = 1,
+					width = 190,
+					height = 190,
+					frame_count = 1,
+					variation_count = 1,
+					axially_symmetrical = false,
+					direction_count = 4,
+					shift = util.by_pixel(3.5, 1.5),
+					scale = 0.5
+				}
+			}
+		}
+	}--[[@as data.CorpsePrototype[] ]]
+end
+
+---@type table<PM.belt_colours,{[1]:string,[2]:string,[3]:string}>
+local order_table = {
+  ["red"]			= {"b-a-a", "b-b-a", "b-c-a"},
+  ["orange"]	= {"b-a-b", "b-b-b", "b-c-b"},
+  ["yellow"]	= {"b-a-c", "b-b-c", "b-c-c"},
+  ["green"]		= {"b-a-d", "b-b-d", "b-c-d"},
+  ["blue"]		= {"b-a-e", "b-b-e", "b-c-e"},
+  ["purple"]	= {"b-a-f", "b-b-f", "b-c-f"},
+}
+
+for colour in pairs(coloured) do
+	local colour_order = order_table[colour]
+	data:extend(make_remnants(colour, colour_order))
 end
