571a6490b206576a6e4701ad2e1f7d27a28268bd
MrSmoothieHuman <stnlwbbr@gmail.com>
2024-10-26 19:27:59 +0100

    added silicon code to element file, needs icons + renders

--- prototype/elements/014-silicon.lua
+++ prototype/elements/014-silicon.lua
@@ -1,7 +1,275 @@
+local PM = require("__periodic-madness__/library")
 data:extend({
 
     {
         type = "item",
-        name = ""
+        name = "pm-low-quality-quartzite",
+        icon_size = 64,
+        icon = "__periodic-madness__/graphics/icons/ores/low-quality-quartzite.png",
+        subgroup = "pm-silicon-mt",
+        order = "a",
+        stack_size = 200
+    },
+    {
+        type = "item",
+        name = "pm-high-quality-quartzite",
+        icon_size = 64,
+        icon = "__periodic-madness__/graphics/icons/ores/high-quality-quartzite.png",
+        subgroup = "pm-silicon-mt",
+        order = "b",
+        stack_size = 200
+    },
+    {
+        type = "item",
+        name = "pm-ferrosilicon",
+        icon_size = 64,
+        icon = "__periodic-madness__/graphics/icons/ores/ferrosilicon.png",
+        subgroup = "pm-silicon-mt",
+        order = "c",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "pm-low-quality-polysilicon",
+        icon_size = 64,
+        icon = "__periodic-madness__/graphics/icons/ores/low-quality-polysilicon.png",
+        subgroup = "pm-silicon-mt",
+        order = "d",
+        stack_size = 400
+    },
+    {
+        type = "item",
+        name = "pm-medium-quality-polysilicon",
+        icon_size = 64,
+        icon = "__periodic-madness__/graphics/icons/ores/medium-quality-polysilicon.png",
+        subgroup = "pm-silicon-mt",
+        order = "e",
+        stack_size = 300
+    },
+    {
+        type = "item",
+        name = "pm-high-quality-polysilicon",
+        icon_size = 64,
+        icon = "__periodic-madness__/graphics/icons/ores/high-quality-polysilicon.png",
+        subgroup = "pm-silicon-mt",
+        order = "f",
+        stack_size = 200
+    },
+
+    {
+        type = "recipe",
+        name = "pm-quartzite-leeching",
+        enabled = false,
+        energy_required = 12,
+        category = "pm-acids",
+        subgroup = "pm-silicon-mt",
+        order = "a",
+        allow_productivity = true,
+        ingredients =
+        {
+            PM.ingredient("pm-chromic-acid", 20, "fluid"),
+            PM.ingredient("pm-metalloid-ore", 16)
+        },
+        results = 
+        {
+            PM.product_range("pm-high-quality-quartzite", 2, 6),
+            PM.product_range("pm-high-quality-quartzite", 4, 12),
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-high-quality-quartzite-seperation",
+        enabled = false,
+        energy_required = 6,
+        category = "chemistry",
+        subgroup = "pm-silicon-mt",
+        order = "b",
+        ingredients = 
+        {
+            PM.ingredient("pm-high-quality-quartzite", 6),
+            PM.ingredient("pm-filled-flux-container", 3),
+            PM.ingredient("pm-coke", 6),
+            PM.ingredient("pm-ferrum", 9)
+        },
+        results = 
+        {
+            PM.product("pm-ferrosilicon", 3),
+            PM.product_range("pm-carbon-monoxide", 5, 8)
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-low-quality-quartzite-seperation",
+        enabled = false,
+        energy_required = 6,
+        category = "pm-crushing",
+        subgroup = "pm-silicon-mt",
+        order = "c",
+        ingredients = 
+        {
+            PM.ingredient("pm-low-quality-quartzite", 6),
+        },
+        results = 
+        {
+            PM.product_range_chance("pm-silicon-ore", 1, 4, 0.5),
+            PM.product_range("pm-ferrum", 2, 6)
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-trichlorosilane",
+        enabled = false,
+        energy_required = 6,
+        category = "chemistry",
+        subgroup = "pm-silicon-mt",
+        order = "d",
+        ingredients = 
+        {
+            PM.ingredient("ferrosilicon", 6),
+            PM.ingredient("pm-hydrogen-chloride", 60, "fluid")
+        },
+        results = 
+        {
+            PM.product("pm-trichlorosilane", 10, "fluid"),
+            PM.product_range("pm-silicon-tetrachloride", 5, 10, "fluid")
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-silicon-tetrachloride-handling",
+        enabled = false,
+        energy_required = 9,
+        category = "chemistry",
+        subgroup = "pm-silicon-mt",
+        order = "da",
+        ingredients = 
+        {
+            PM.ingredient("pm-silicon-tetrachloride", 10, "fluid"),
+            PM.ingredient("pm-hydrogen-gas", 5, "fluid"),
+            PM.ingredient("pm-vanadium-oxide-catalyst", 2)
+        },
+        results = 
+        {
+            PM.product_range("pm-trichlorosilane", 4, 7),
+            PM.catalyst_range("pm-catalyst-container", 1, 2, 2)
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-silane",
+        enabled = false,
+        energy_required = 3,
+        category = "chemistry",
+        subgroup = "pm-silicon-mt",
+        order = "e",
+        ingredients = 
+        {
+            PM.ingredient("pm-trichlorosilane", 10,"fluid"),
+            PM.ingredient("pm-aluminium-plate", 10) --catalyst for hard mode
+        },
+        results = 
+        {
+            PM.ingredient("pm-silane", 5),
+            PM.product_range("pm-silicon-tetrachloride", 2.5, 5, "fluid")
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-indsutrial-grade-silicon",
+        enabled = false,
+        energy_required = 3,
+        category = "chemistry",
+        subgroup = "pm-silicon-mt",
+        order = "f",
+        ingredients = 
+        {
+            PM.ingredient("pm-silane", 5, "fluid"),
+            PM.ingredient("pm-argon-gas", 15, "fluid")
+        },
+        results = 
+        {
+            PM.product_range("pm-silicon-ore", 3, 5),
+            PM.product_range_chance("pm-argon-gas", 2, 8, 0.65, "fluid"),
+            PM.product_range("pm-hydrogen-gas", 4, 8, "fluid")
+        }
+    },
+
+    {
+        type = "recipe",
+        name = "pm-low-quality-polysilicon",
+        enabled = false,
+        energy_required = 5,
+        category = "chemistry",
+        subgroup = "pm-silicon-mt",
+        order = "g",
+        ingredients = 
+        {
+            PM.ingredient("pm-silane", 15, "fluid"),
+            PM.ingredient("pm-nitrogen-gas", 60, "fluid"),
+            PM.ingredient("pm-silicon-ore", 10)
+        },
+        results = 
+        {
+            PM.product_chance("pm-low-quality-polysilicon", 2, 0.95),
+            PM.product_range("pm-silicon-ore", 0, 5)
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-low-to-med-quality-polysilicon",
+        enabled = false,
+        energy_required = 10,
+        category = "chemistry",
+        subgroup = "pm-silicon-mt",
+        order = "h",
+        ingredients = 
+        {
+            PM.ingredient("pm-silane", 10, "fluid"),
+            PM.ingredient("pm-nitrogen-gas", 60, "fluid"),
+            PM.ingredient("pm-low-quality-polysilicon", 5)
+        },
+        results = 
+        {
+            PM.product_chance("pm-medium-quality-polysilicon", 2, 0.85),
+            PM.product_range("pm-low-quality-polysilicon", 0, 5)
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-med-to-high-quality-polysilicon",
+        enabled = false,
+        energy_required = 20,
+        category = "chemistry",
+        subgroup = "pm-silicon-mt",
+        order = "i",
+        ingredients = 
+        {
+            PM.ingredient("pm-silane", 5, "fluid"),
+            PM.ingredient("pm-nitrogen-gas", 60, "fluid"),
+            PM.ingredient("pm-medium-quality-polysilicon", 5)
+        },
+        results = 
+        {
+            PM.product_chance("pm-high-quality-polysilicon", 2, 0.75),
+            PM.product_range("pm-medium-quality-polysilicon", 0, 5)
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-silicon-rod-forming",
+        enabled = false,
+        energy_required = 18,
+        category = "pm-moltening",
+        subgroup = "pm-silicon-mt",
+        order = "j",
+        ingredients = 
+        {
+            PM.ingredient("pm-high-quality-silicon", 4),
+            PM.ingredient("steam", 50, "fluid"),
+        },
+        results = 
+        {
+            PM.product("pm-polysilicon-rod", 1)
+        }
     }
 })
