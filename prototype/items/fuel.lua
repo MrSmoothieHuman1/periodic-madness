eb6e75be312bb740ffb3ee8a7053b69068b1ba97
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-01-07 19:04:56 +0000

    changed petrol canister to 50 stack size and 3MJ, buffed their acceleration multiplier to 1.75

--- prototype/items/fuel.lua
+++ prototype/items/fuel.lua
@@ -30,14 +30,14 @@ data:extend({
     name = "pm-petrol-canister",
     icon_size = 64,
     icon = "__periodic-madness__/graphics/icons/petrol-canister.png",
-    fuel_value = "1.5MJ",
+    fuel_value = "3MJ",
     fuel_category = "pm-oil-canisters",
-    fuel_acceleration_multiplier = 1.5,
+    fuel_acceleration_multiplier = 1.75,
     fuel_top_speed_multiplier = 2,
     subgroup = "pm-fuels",
     order = "d",
     burnt_result = "pm-empty-fuel-canister",
-    stack_size = 10
+    stack_size = 50
   },
   {
     type = "item",
@@ -61,7 +61,7 @@ data:extend({
     fuel_value = "32MJ",
     fuel_category = "pm-oil-canisters",
     fuel_acceleration_multiplier = 1,
-    fuel_top_speed_multiplier = 1.05,
+    fuel_top_speed_multiplier = 1,
     subgroup = "pm-fuels",
     order = "f",
     burnt_result = "pm-empty-fuel-canister",
@@ -116,8 +116,6 @@ data.raw["recipe"]["rocket-fuel"].subgroup = "pm-fuels"
 data.raw["recipe"]["rocket-fuel"].order = "g"
 data.raw["item"]["nuclear-fuel"].subgroup = "pm-fuels"
 data.raw["item"]["nuclear-fuel"].order = "h"
-data.raw["recipe"]["nuclear-fuel"].subgroup = "pm-fuels"
-data.raw["recipe"]["nuclear-fuel"].order = "h"
 
 table.insert(data.raw["inserter"]["burner-inserter"].energy_source.fuel_categories, "pm-oil-canisters")
 table.insert(data.raw["car"]["car"].energy_source.fuel_categories, "pm-oil-canisters")
