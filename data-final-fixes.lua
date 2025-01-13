ec88534c4b182726850a6f838dcc7086a3933fad
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-01-13 00:51:21 +0000

    changed explosives into capsules

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,3 +2,8 @@ require("compatibility.K2")
 require("compatibility.BZ.foundry")
 require("compatibility.BZ.zirconium")
 require("compatibility.colour-theory-circuits")
+
+local throwplosive = data.raw.item.explosives
+data.raw.item.explosives = nil
+throwplosive.type = "capsule"
+data:extend({item})
