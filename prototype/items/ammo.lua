f4bd6e558ed7b926bdfe4df03ae5d93f0f59c9b1
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-01-13 01:18:31 +0000

    lowered stack sizes on slugs

--- prototype/items/ammo.lua
+++ prototype/items/ammo.lua
@@ -236,7 +236,7 @@ data:extend({
   magazine_size = 6,
   subgroup = "pm-shotgun-ammo",
   order = "aa",
-  stack_size = 200
+  stack_size = 100
 },
 {
   type = "ammo",
@@ -283,7 +283,7 @@ data:extend({
   magazine_size = 6,
   subgroup = "pm-shotgun-ammo",
   order = "ba",
-  stack_size = 200
+  stack_size = 100
 },
 {
   type = "ammo",
@@ -377,7 +377,7 @@ data:extend({
   magazine_size = 8,
   subgroup = "pm-shotgun-ammo",
   order = "da",
-  stack_size = 200
+  stack_size = 150
 },
 
 {
