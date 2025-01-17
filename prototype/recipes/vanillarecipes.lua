a43834b6d14552605c7f2ac945d60485942896eb
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-01-17 23:54:46 +0000

    made ass 3's cheaper, changed cooling plating to 1 coolant

--- prototype/recipes/vanillarecipes.lua
+++ prototype/recipes/vanillarecipes.lua
@@ -167,11 +167,11 @@ data.raw["recipe"]["concrete"].ingredients =
 data.raw["recipe"]["assembling-machine-3"].ingredients =
 {
   PM.ingredient("assembling-machine-2", 1),
-  PM.ingredient("electric-engine-unit", 3),
+  PM.ingredient("electric-engine-unit", 2),
   PM.ingredient("processing-unit", 1),
-  PM.ingredient("pm-cooling-plating", 10),
+  PM.ingredient("pm-coolant", 1, "fluid"),
   PM.ingredient("pm-sapphire-laser-head", 1),
-  PM.ingredient("pm-radiation-resistant-panelling", 10)
+  PM.ingredient("pm-radiation-resistant-panelling", 5)
 }
 data.raw["recipe"]["oil-refinery"].ingredients =
 {
