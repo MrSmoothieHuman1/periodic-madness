02c364a1ff6e6bd9406bb0161e31a047d67924fa
PennyJim <40438818+PennyJim@users.noreply.github.com>
2025-01-18 21:17:02 -0700

    Mark coolant balance info

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -17,12 +17,21 @@ data:extend{
 		category = "pm-reactor-coolant-burning",
 		-- We want the contents of the recipe to be how much fluid it takes to cool it for 1kJ
 		-- That means the 'time' of the recipe needs to be 1 second
-		energy_required = 1,
+
+		-- And because of the sheer quantity of energy a reactor puts off,
+		-- we want coolant to be able to cool *at least* 10kJ per unit of fluid
+		-- But that's *super* thirsty (like 100/s for 1MW)
+
+		-- I just checked and it consumes
+		--	4000/s for 40MW when it's 10x
+		--	400/s for 40MW when it's 100x
+		-- 	80/s for 40MW when it's 500x
+		energy_required = 100,
 		ingredients = {
 			PM.ingredient("water", 1, "fluid")
 		},
 		results = {
-			PM.product("pm-liquid-heat", 1, "fluid")
+			PM.product("pm-liquid-heat", 100, "fluid")
 		}
 	}--[[@as data.RecipePrototype]],
 	{
