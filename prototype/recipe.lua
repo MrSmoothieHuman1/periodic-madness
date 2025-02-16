8546ba59976ea36cb08b19ec24c6d15e0aa7c93c
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-02-16 16:27:42 +0000

    doubled amount of barium needed for AEM packs, need to clean up ratios

--- prototype/recipe.lua
+++ prototype/recipe.lua
@@ -554,7 +554,7 @@ data:extend({
       PM.ingredient("pm-calcium-ore", 25),
       PM.ingredient("pm-beryllium-ore", 8),
       PM.ingredient("pm-magnesium-ore", 4),
-      PM.ingredient("pm-barium-ore", 2),
+      PM.ingredient("pm-barium-ore", 4),
       PM.ingredient("pm-science-bottle", 1)
     },
     enabled = false,
