8fed7b1079941bc934c18f4bb9b271330ec443c6
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-03-05 14:46:03 +0000

    added some funny stuff, now slightly understand runtime

--- control.lua
+++ control.lua
@@ -4,4 +4,5 @@ event_handler.add_libraries{
 	require("runtime.remote-interface"),
 	require("runtime.reactor-cooling"),
 	require("runtime.freeplay-interface"),
+	require("runtime.periodic-fun")
 }
