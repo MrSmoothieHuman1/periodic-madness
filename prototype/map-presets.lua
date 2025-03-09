d1748eb3291b12d435eba8e9818d135ab86c7f46
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-03-09 12:18:53 +0000

    added a map-preset file, currnetly just has railworld in it

---
+++ prototype/map-presets.lua
@@ -0,0 +1,60 @@
+data:extend({
+
+    ["pm-periodic-default"] =
+    {
+      order = "e",
+      basic_settings =
+      {
+        property_expression_names = {},
+        autoplace_controls =
+        {
+          coal =
+          {
+            frequency = 0.33333333333,
+            size = 3
+          },
+          ["copper-ore"] =
+          {
+            frequency = 0.33333333333,
+            size = 3
+          },
+          ["crude-oil"] =
+          {
+            frequency = 0.33333333333,
+            size = 3
+          },
+          ["uranium-ore"] =
+          {
+            frequency = 0.33333333333,
+            size = 3
+          },
+          ["iron-ore"] =
+          {
+            frequency = 0.33333333333,
+            size = 3
+          },
+          stone =
+          {
+            frequency = 0.33333333333,
+            size = 3
+          },
+          ["enemy-base"] =
+          {
+            size = 1
+          },
+          water = {frequency = 0.5, size = 1.5}
+        },
+      },
+      advanced_settings =
+      {
+        enemy_evolution =
+        {
+          time_factor = 0.000002
+        },
+        enemy_expansion =
+        {
+         enabled = false
+        }
+      }
+    },
+})
