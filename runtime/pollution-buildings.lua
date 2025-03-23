31fff7b29176c8583930e28880b744b91be47d66
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-03-23 20:25:01 +0000

    thank you penny for helping fix it!!

--- runtime/pollution-buildings.lua
+++ runtime/pollution-buildings.lua
@@ -1,8 +1,9 @@
 --credit to quezeler for helping me out on this, and giving me the code
 
 ---@type event_handler
+local handler = {on_nth_tick = {}}
 --MARK: No pollution
-script.on_nth_tick(60 * 10, function(event) --checks every 10 seconds
+handler.on_nth_tick[60 * 10] = function(event) --checks every 10 seconds
     for _, surface in pairs(game.surfaces) do
       local entities = surface.find_entities_filtered
       {
@@ -25,6 +26,8 @@ script.on_nth_tick(60 * 10, function(event) --checks every 10 seconds
         end
       end
     end
-  end)
+  end
+
+return handler
 
   --MARK: Only pollution
