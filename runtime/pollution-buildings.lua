42ad63182ea619e30d8f9c0035297b7338d54d7c
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-03-24 20:11:44 +0000

    added list of pollution value > colour list

--- runtime/pollution-buildings.lua
+++ runtime/pollution-buildings.lua
@@ -2,6 +2,19 @@
 
 ---@type event_handler
 local handler = {on_nth_tick = {}}
+
+--pollution value to colour list:
+--0-50: Lowest colour
+--55-65: Second colour
+--65-75: Third colour
+--75-85: Fourth colour
+--85-95: Fifth colour
+--95-105: Sitxh colour
+--105-115: Seventh colour
+--115-125: Eighth colour
+--125-135: Nineth colour
+--135-145: Tenth colour
+--145-150+: Eleventh colour (max)
 --MARK: No pollution
 handler.on_nth_tick[60 * 2] = function(event) --checks every 2 seconds
     for _, surface in pairs(game.surfaces) do
@@ -11,9 +24,8 @@ handler.on_nth_tick[60 * 2] = function(event) --checks every 2 seconds
       }
       for _, entity in ipairs(entities) do
         if surface.get_pollution(entity.position) > 60 then
-        --15 pollution in a til is the min. amount needed to have it spread
-        --60 pollution is when trees start dying - makes sense thematically, but might be too low?
-        --TODO: add list of pollution required to make chunk more red
+          --15 pollution in a til is the min. amount needed to have it spread
+          --60 pollution is when trees start dying - makes sense thematically, but might be too low?
           entity.disabled_by_script = true
           entity.disabled_by_script = true
           -- Show alert
