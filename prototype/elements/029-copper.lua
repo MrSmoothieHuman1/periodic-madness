1a65b9f76191defdaf052ee2dc290e78404479b2
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-03-29 19:39:28 +0000

    added recipes for advanced copper

--- prototype/elements/029-copper.lua
+++ prototype/elements/029-copper.lua
@@ -29,6 +29,48 @@ data:extend({
     order = "b",
     stack_size = 200
   },
+  {
+    type = "item",
+    name = "pm-copper-powder",
+    icon_size = 64,
+    icon = "__periodic-madness__/graphics/icons/ores/copper-chunks.png",
+    pictures =
+    {
+      { filename = "__periodic-madness__/graphics/icons/ores/copper-chunks.png",   size = 64, scale = 0.5 },
+      { filename = "__periodic-madness__/graphics/icons/ores/copper-chunks-2.png", size = 64, scale = 0.5 }
+    },
+    subgroup = "pm-advanced-copper-tm",
+    order = "a",
+    stack_size = 300
+  },
+  {
+    type = "item",
+    name = "pm-blister-copper",
+    icon_size = 64,
+    icon = "__periodic-madness__/graphics/icons/ores/copper-chunks.png",
+    pictures =
+    {
+      { filename = "__periodic-madness__/graphics/icons/ores/copper-chunks.png",   size = 64, scale = 0.5 },
+      { filename = "__periodic-madness__/graphics/icons/ores/copper-chunks-2.png", size = 64, scale = 0.5 }
+    },
+    subgroup = "pm-advanced-copper-tm",
+    order = "c",
+    stack_size = 150
+  },
+  {
+    type = "item",
+    name = "pm-fire-refined-copper",
+    icon_size = 64,
+    icon = "__periodic-madness__/graphics/icons/ores/copper-chunks.png",
+    pictures =
+    {
+      { filename = "__periodic-madness__/graphics/icons/ores/copper-chunks.png",   size = 64, scale = 0.5 },
+      { filename = "__periodic-madness__/graphics/icons/ores/copper-chunks-2.png", size = 64, scale = 0.5 }
+    },
+    subgroup = "pm-advanced-copper-tm",
+    order = "d",
+    stack_size = 200
+  },
 
   {
     type = "recipe",
@@ -130,4 +172,123 @@ data:extend({
 
 --MARK: Adv. Copper
 -- better version of copper, normal chunk to ore is 1:2 - this is 1:6, so 4 chunks turns into 24 ore
+
+{
+  type = "recipe",
+  name = "pm-copper-powder",
+  enabled = false,
+  energy_required = 4,
+  category = "pm-crushing",
+  subgroup = "pm-advanced-copper-tm",
+  order = "a",
+  ingredients =
+  {
+    PM.ingredient("pm-copper-chunk", 4),
+    PM.ingredient("pm-patina", 3)
+  },
+  results = 
+  {
+    PM.product("pm-copper-powder", 12)
+  }
+},
+{
+  type = "recipe",
+  name = "pm-powdered-copper-frothing",
+  enabled = false,
+  energy_required = 2,
+  category = "pm-mixing",
+  subgroup = "pm-advanced-copper-tm",
+  order = "b",
+  ingredients =
+  {
+    PM.ingredient("pm-copper-powder", 6),
+    PM.ingredient("water", 30, "fluid"),
+    PM.ingredient("pm-sodium-hydroxide", 5, "fluid")
+  },
+  results =
+  {
+    PM.product("pm-copper-froth", 10, "fluid"),
+    PM.product("pm-copper-tailings", 15, "fluid")
+  }
+},
+{
+  type = "recipe",
+  name = "pm-copper-matte",
+  enabled = false,
+  energy_required = 8,
+  category = "pm-moltening", --should be in reverbatory furnace
+  subgroup = "pm-advanced-copper-tm",
+  order = "c",
+  main_product = "pm-copper-matte",
+  ingredients =
+  {
+    PM.ingredient("pm-copper-froth", 10),
+  },
+  results =
+  {
+    PM.product("pm-copper-matte", 15, "fluid"),
+    PM.product_range("sulfur", 0, 4),
+    PM.product_range_chance("pm-gold-chunk", 0, 2, 0.66)
+  }
+},
+{
+  type = "recipe",
+  name = "pm-blister-copper",
+  enabled = false,
+  energy_required = 6,
+  category = "pm-moltening",
+  subgroup = "pm-advanced-copper-tm",
+  order = "d",
+  main_product = "pm-blister-copper",
+  ingredients =
+  {
+    PM.ingredient("pm-copper-matte", 15, "fluid"),
+    PM.ingredient("pm-cobalt-catalyst", 3)
+  },
+  results = 
+  {
+    PM.catalyst_range_chance("pm-cobalt-catalyst", 1, 3, 1, 0.8),
+    PM.catalyst_range_chance("pm-catalyst-container", 0, 2, 0, 0.2),
+    PM.product("pm-blister-copper", 14, "fluid"),
+    PM.product_range("pm-combined-ore-slag", 0, 3, "fluid")
+  }
+},
+{
+  type = "recipe",
+  name = "pm-fire-refined-copper",
+  enabled = false,
+  energy_required = 4,
+  category = "pm-coldening",
+  subgroup = "pm-advanced-copper-tm",
+  order = "e",
+  ingredients =
+  {
+    PM.ingredient("pm-blister-copper", 14, "fluid"),
+    PM.ingredient("pm-syngas", 40, "fluid")
+  },
+  results = 
+  {
+    PM.product("pm-fire-refined-copper", 10)
+  }
+},
+{
+  type = "recipe",
+  name = "pm-fire-refined-copper-seperation",
+  enabled = false,
+  energy_required = 4,
+  category = "pm-electrolysis",
+  subgroup = "pm-advanced-copper-tm",
+  order = "f",
+  main_product = "copper-ore",
+  ingredients =
+  {
+    PM.ingredient("pm-fire-refined-copper", 5),
+    PM.ingredient("oxygen-gas", 20, "fluid")
+  },
+  results =
+  {
+    PM.product("copper-ore", 12),
+    PM.product_range_chance("pm-platinum-chunk", 0, 2, 0.33)
+  }
+}
 })
