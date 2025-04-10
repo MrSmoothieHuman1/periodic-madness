61e8976a6efe8893f03bc875abe9fe4ba5ba812d
MrSmoothieHuman <stnlwbbr@gmail.com>
2025-04-10 16:54:25 +0100

    finally gave quality modules their correct icons

---
+++ compatibility/quality.lua
@@ -0,0 +1,15 @@
+if mods["quality"] then
+    data.raw["technology"]["modules"].icon_size = "__periodic-madness__/graphics/technology/modules-quality.png"
+    data.raw["module"]["quality-module"].icon_size = 64
+    data.raw["module"]["quality-module"].icon = "__periodic-madness__/graphics/icons/modules/quality-module-1.png"
+    data.raw["module"]["quality-module-2"].icon_size = 64
+    data.raw["module"]["quality-module-2"].icon = "__periodic-madness__/graphics/icons/modules/quality-module-2.png"
+    data.raw["module"]["quality-module-3"].icon_size = 64
+    data.raw["module"]["quality-module-3"].icon = "__periodic-madness__/graphics/icons/modules/quality-module-3.png"
+    data.raw["technology"]["quality-module"].icon_size = 256
+    data.raw["technology"]["quality-module"].icon = "__periodic-madness__/graphics/icons/technology/quality-module-1-tech.png"
+    data.raw["technology"]["quality-module-2"].icon_size = 256
+    data.raw["technology"]["quality-module-2"].icon = "__periodic-madness__/graphics/technology/quality-module-2-tech.png"
+    data.raw["technology"]["quality-module-3"].icon_size = 256
+    data.raw["technology"]["quality-module-3"].icon = "__periodic-madness__/graphics/technology/quality-module-3-tech.png"
+end
