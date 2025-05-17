d37942282248609139f23dde6086243f95ab082c
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-05-17 07:15:25 +0100

    moved stack inserters to PTM/RNM

--- compatibility/stack-inserters.lua
+++ compatibility/stack-inserters.lua
@@ -3,18 +3,9 @@ local PM = require("library")
 if mods["stack-inserters"] then
 
         --MARK: Tech changes
-    data.raw["technology"]["stack-inserter"].prerequisites = {"bulk-inserter"}
-    data.raw["technology"]["stack-inserter"].unit.count = 250
+    data.raw["technology"]["stack-inserter"].prerequisites = {"bulk-inserter", "processing-unit", "lubrication"}
+    data.raw["technology"]["stack-inserter"].unit.count = 400
     data.raw["technology"]["stack-inserter"].unit.ingredients =
-    {
-        {"automation-science-pack", 1},
-        {"logistic-science-pack", 1},
-        {"pm-advanced-advanced-transition-metal-science-pack", 1}
-    }
-
-    data.raw["technology"]["transport-belt-capacity-1"].prerequisites = {"pm-post-transition-metal-pack-unlock", "chemical-science-pack", "stack-inserter"}
-    data.raw["technology"]["transport-belt-capacity-1"].unit.count = 500
-    data.raw["technology"]["transport-belt-capacity-1"].unit.ingredients =
     {
         {"automation-science-pack", 1},
         {"logistic-science-pack", 1},
@@ -22,9 +13,10 @@ if mods["stack-inserters"] then
         {"pm-post-transition-metal-science-pack", 1},
         {"chemical-science-pack", 1}
     }
-    data.raw["technology"]["transport-belt-capacity-2"].prerequisites = {"pm-metalloid-pack-unlock", "pm-alkali-metal-pack-unlock", "transport-belt-capacity-1"}
-    data.raw["technology"]["transport-belt-capacity-2"].unit.count = 1000
-    data.raw["technology"]["transport-belt-capacity-2"].unit.ingredients =
+
+    data.raw["technology"]["transport-belt-capacity-1"].prerequisites = {"pm-post-transition-metal-pack-unlock", "chemical-science-pack", "stack-inserter"}
+    data.raw["technology"]["transport-belt-capacity-1"].unit.count = 800
+    data.raw["technology"]["transport-belt-capacity-1"].unit.ingredients =
     {
         {"automation-science-pack", 1},
         {"logistic-science-pack", 1},
@@ -34,8 +26,21 @@ if mods["stack-inserters"] then
         {"pm-metalloid-science-pack", 1},
         {"pm-alkali-science-pack", 1}
     }
+    data.raw["technology"]["transport-belt-capacity-2"].prerequisites = {"pm-metalloid-pack-unlock", "pm-alkali-metal-pack-unlock", "transport-belt-capacity-1"}
+    data.raw["technology"]["transport-belt-capacity-2"].unit.count = 1600
+    data.raw["technology"]["transport-belt-capacity-2"].unit.ingredients =
+    {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"pm-advanced-advanced-transition-metal-science-pack", 1},
+        {"pm-post-transition-metal-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"pm-metalloid-science-pack", 1},
+        {"pm-alkali-science-pack", 1},
+        {"pm-alkaline-earth-metal-science-pack", 1}
+    }
     data.raw["technology"]["transport-belt-capacity-3"].prerequisites = {"pm-alkaline-metal-pack-unlock", "transport-belt-capacity-2"}
-    data.raw["technology"]["transport-belt-capacity-3"].unit.count = 2000
+    data.raw["technology"]["transport-belt-capacity-3"].unit.count = 3200
     data.raw["technology"]["transport-belt-capacity-3"].unit.ingredients =
     {
         {"automation-science-pack", 1},
@@ -48,7 +53,7 @@ if mods["stack-inserters"] then
         {"pm-alkaline-earth-metal-science-pack", 1}
     }
     data.raw["technology"]["transport-belt-capacity-4"].prerequisites = {"transport-belt-capacity-3"}
-    data.raw["technology"]["transport-belt-capacity-4"].unit.count = 4000
+    data.raw["technology"]["transport-belt-capacity-4"].unit.count = 6400
     data.raw["technology"]["transport-belt-capacity-4"].unit.ingredients =
     {
         {"automation-science-pack", 1},
@@ -65,11 +70,12 @@ if mods["stack-inserters"] then
     data.raw["inserter"]["stack-inserter"].rotation_speed = 0.014
 
     --MARK: Recipe changes
+    data.raw["recipe"]["stack-inserter"].category = "crafting-with-fluid"
     data.raw["recipe"]["stack-inserter"].ingredients =
     {
         PM.ingredient("bulk-inserter", 1),
-        PM.ingredient("pm-solid-lubricant", 3),
-        PM.ingredient("advanced-circuit", 2)
+        PM.ingredient("pm-heavy-lubricant", 5, "fluid"),
+        PM.ingredient("processing-unit", 2)
     }
     data.raw["recipe"]["stack-inserter"].energy_required = 1
 end
