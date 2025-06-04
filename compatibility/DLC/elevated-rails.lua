6f1a93ee63d09e518153b4e139036e1308d293eb
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-06-04 23:16:18 +0100

    fixed packs needed for elevated rails + syntax issues

--- compatibility/DLC/elevated-rails.lua
+++ compatibility/DLC/elevated-rails.lua
@@ -1,20 +1,19 @@
-if feature_flags["rail-bridges"] then
+local PM = require("library")
+if feature_flags["rail_bridges"] then
 
     data.raw["technology"]["elevated-rail"].unit.ingredients =
     {
         {"automation-science-pack", 1},
         {"logistic-science-pack", 1},
         {"pm-advanced-advanced-transition-metal-science-pack", 1},
-        {"chemical-science-pack", 1},
         {"pm-post-transition-metal-science-pack", 1},
-        {"pm-alkali-metal-science-pack", 1},
     }
     data.raw["technology"]["elevated-rail"].prerequisites = {"railway", "pm-post-transition-metal-pack-unlock"}
 
     data.raw["recipe"]["rail-support"].energy_required = 2
     data.raw["recipe"]["rail-support"].ingredients = 
     {
-        PM.ingredient("pm-refined-concrete", 10),
+        PM.ingredient("refined-concrete", 10),
         PM.ingredient("pm-heavyweight-framing", 20),
         PM.ingredient("pm-lightweight-framing", 15)
     }
@@ -23,6 +22,6 @@ if feature_flags["rail-bridges"] then
     {
         PM.ingredient("rail", 8),
         PM.ingredient("pm-lightweight-framing", 15),
-        PM.ingredient("pm-refined-concrete", 25)
+        PM.ingredient("refined-concrete", 25)
     }
 end
