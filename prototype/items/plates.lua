2f18783ad66758c16ce2c68ed7743f00fb1b712b
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-07-04 22:17:54 +0100

    improved graphics of uhrlumin, added extra variation

--- prototype/items/plates.lua
+++ prototype/items/plates.lua
@@ -277,6 +277,11 @@ data:extend({
     name = "pm-uhrlumin-plate",
     icon_size = 64,
     icon = "__periodic-madness__/graphics/icons/plates/uhrlumin-plate.png",
+    pictures =
+    {
+      {filename = "__periodic-madness__/graphics/icons/plates/uhrlumin-plate.png", size = 64, scale = 0.5},
+      {filename = "__periodic-madness__/graphics/icons/plates/uhrlumin-plate-2.png", size = 64, scale = 0.5},
+    },
     subgroup = "pm-magnesium-aem",
     order = "d",
     stack_size = 100
