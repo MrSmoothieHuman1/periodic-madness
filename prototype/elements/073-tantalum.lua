82911f929729befc7879ba6abe3182d639a24b60
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-07-21 13:11:13 +0100

    added base chain for tantalum/niobium

--- prototype/elements/073-tantalum.lua
+++ prototype/elements/073-tantalum.lua
@@ -19,5 +19,65 @@ data:extend({
         subgroup = "pm-tantalum-tm",
         order = "b",
         stack_size = 400,
-    }
+    },
+
+    {
+        type = "recipe",
+        name = "pm-coltan",
+        enabled = false,
+        energy_required = 6,
+        category = "pm-acids",
+        subgroup = "pm-tantalum-tm",
+        order = "a",
+        allow_productivity = true,
+        ingredients = 
+        {
+            PM.ingredient("pm-transition-metals-ore", 16),
+            PM.ingredient("pm-chromic-acid", 25)
+        },
+        results = 
+        {
+            PM.product("pm-coltan", 4)
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-coltan-powder",
+        enabled = false,
+        energy_required = 4,
+        category = "pm-crushing",
+        subgroup = "pm-tantalum-tm",
+        order = "b",
+        allow_productivity = true,
+        ingredients =
+        {
+            PM.ingredient("pm-coltan", 4)
+        },
+        results =
+        {
+            PM.product("pm-coltan-powder", 10)
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-tantalite-niobite-tailings",
+        enabled = false,
+        energy_required = 8,
+        category = "pm-mixing",
+        subgroup = "pm-tantalum-tm",
+        order = "c",
+        ingredients = 
+        {
+            PM.ingredient("pm-coltan-powder", 10),
+            PM.ingredient("pm-sulfuric-acid", 5, "fluid"),
+            PM.ingredient("pm-hydrochloric-acid", 10, "fluid")
+        },
+        results = 
+        {
+            PM.product("pm-tantalite-niobite-tailings", 15, "fluid")
+        }
+    },
+
+    --actual tantalum
+
 })
