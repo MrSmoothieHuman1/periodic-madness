d90dd54cb83cc872165c614f1fedf10768cb6f62
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-07-21 01:15:33 +0100

    added items to tantalum file

--- prototype/elements/073-tantalum.lua
+++ prototype/elements/073-tantalum.lua
@@ -1,1 +1,21 @@
 local PM = require("library")
+
+data:extend({
+
+    {
+        type = "item",
+        name = "pm-coltan",
+        icon_size = 64,
+        icon = "__periodic-madness__/graphics/icons/placeholder-item.png",
+        subgroup = "pm-tantalum-tm",
+        order = "a"
+    },
+    {
+        type = "item",
+        name = "pm-coltan-powder",
+        icon_size = 64,
+        icon = "__periodic-madness__/graphics/icons/placeholder-item.png",
+        subgroup = "pm-tantalum-tm",
+        order = "b"
+    }
+})
