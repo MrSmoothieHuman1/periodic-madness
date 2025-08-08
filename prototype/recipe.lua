bf958c255ca3a4e416f3a3ccd907b17b799703fe
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-08-08 10:20:29 +0100

    forgot to make light lube EEU recipe actually take light lubes

--- prototype/recipe.lua
+++ prototype/recipe.lua
@@ -4245,7 +4245,7 @@ data:extend({
     allow_productivity = true,
     ingredients =
     {
-      PM.ingredient("pm-heavy-lubricant", 5, "fluid"),
+      PM.ingredient("pm-light-lubricant", 5, "fluid"),
       PM.ingredient("pm-gold-wire", 5),
       PM.ingredient("engine-unit", 1),
       PM.ingredient("advanced-circuit", 2),
