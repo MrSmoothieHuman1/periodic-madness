d13e790b92dbf06d70744d555fdf7438481f6c97
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-08-09 02:58:32 +0100

    added chain for ruthenium ore, deciding wether to add Ru plate

---
+++ prototype/elements/044-ruthenium.lua
@@ -0,0 +1,65 @@
+local PM = require("library")
+
+data:extend({
+
+    {
+        type = "recipe",
+        name = "pm-ruthenium-filtered-anode-sludge",
+        enabled = false,
+        energy_required = 15,
+        category = "pm-washing",
+        subgroup = "pm-ruthenium-tm",
+        order = "a",
+        ingredients =
+        {
+            PM.ingredient("pm-filtered-anode-sludge", 10, "fluid"),
+            PM.ingredient("pm-palladium-ore", 5),
+            PM.ingredient("pm-transition-metals-ore", 16)
+        },
+        results = 
+        {
+            PM.product("pm-ruthenium-filtered-anode-sludge", 12, "fluid")
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-ammonium-ruthenium-chloride",
+        enabled = false,
+        energy_required = 10,
+        category = "chemistry",
+        subgroup = "pm-ruthenium-tm",
+        order = "b",
+        ingredients =
+        {
+            PM.ingredient("pm-ruthenium-filtered-anode-sludge", 6, "fluid"),
+            PM.ingredient("pm-ammonium-chloride", 3),
+            PM.ingredient("pm-acetone", 5, "fluid")
+        },
+        results =
+        {
+            PM.product("pm-ammonium-ruthenium-chloride", 4),
+        }
+    },
+    {
+        type = "recipe",
+        name = "pm-ruthenium-ore",
+        enabled = false,
+        energy_required = 7.5,
+        category = "chemistry",
+        subgroup = "pm-ruthenium-tm",
+        order = "c",
+        ingredients =
+        {
+            PM.ingredient("pm-ammonium-ruthenium-chloride", 4),
+            PM.ingredient("pm-hydrogen-gas", 20, "fluid"),
+            PM.ingredient("pm-cobalt-catalyst", 2)
+        },
+        results = 
+        {
+            PM.product("pm-ruthenium-ore", 3),
+            PM.product_range("pm-ammonia-gas", 1.5, 2),
+            PM.product_chance("pm-cobalt-catalyst", 2, 0.85),
+            PM.product_chance("pm-catalyst-container", 2, 0.15)
+        }
+    }
+})
