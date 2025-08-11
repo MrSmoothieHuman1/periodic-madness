a088ee27f2dcda7f8e2f52782e1c6f90fdc3c527
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-08-11 22:47:37 +0100

    fixed air filterers stopping at 1 pollution left in a chunk

--- prototype/entity/pollution-limits.lua
+++ prototype/entity/pollution-limits.lua
@@ -28,12 +28,12 @@ local function limit_pollution(entity, min, max)
 	}--[[@as PollutionData]]
 end
 
---   --15 pollution in a til is the min. amount needed to have it spread
---   --60 pollution is when trees start dying - makes sense thematically, but might be too low?
+--   15 pollution in a til is the min. amount needed to have it spread
+--   60 pollution is when trees start dying - makes sense thematically, but might be too low?
 data:extend{
 	limit_pollution("pm-greenhouse", 0, 60),
 	limit_pollution("pm-factorian-bacteria-greenhouse", 0, 60),
 	limit_pollution("pm-CO2-nullifer", 0, 120),
-	limit_pollution("pm-air-filterer", 1, 1/0),
+	limit_pollution("pm-air-filterer", 0, 1/0),
 }
 return limit_pollution
