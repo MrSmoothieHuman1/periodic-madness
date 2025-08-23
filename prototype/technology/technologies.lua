6bb11897a9243b3c3de701fa96e53e554728096a
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-08-23 20:27:51 +0100

    added technology icon for inconel heating coils

--- prototype/technology/technologies.lua
+++ prototype/technology/technologies.lua
@@ -5102,7 +5102,7 @@ data:extend({
     type = "technology",
     name = "pm-inconel-heating-coils",
     icon_size = 256,
-    icon = "__periodic-madness__/graphics/icons/placeholder-tech.png",
+    icon = "__periodic-madness__/graphics/technology/inconel-625-heating-coils.png",
     effects =
     {
       PM.unlock_recipe("pm-inconel-625-heating-coil")
