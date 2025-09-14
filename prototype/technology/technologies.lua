f0c34a2e33b927e105cfef4a3365295304e68c8b
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-09-14 14:46:31 +0100

    osmium processing tech icon

--- prototype/technology/technologies.lua
+++ prototype/technology/technologies.lua
@@ -4637,7 +4637,7 @@ data:extend({
     type = "technology",
     name = "pm-osmium-processing",
     icon_size = 256,
-    icon = "__periodic-madness__/graphics/icons/placeholder-tech.png",
+    icon = "__periodic-madness__/graphics/technology/osmium-processing.png",
     effects =
     {
       PM.unlock_recipe("pm-osmium-filtered-anode-sludge"),
