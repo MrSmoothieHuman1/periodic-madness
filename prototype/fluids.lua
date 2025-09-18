75da9fc2ba396880f8a381430935637b785c91fe
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-09-18 20:10:23 +0100

    2-phenylpyridine icon

--- prototype/fluids.lua
+++ prototype/fluids.lua
@@ -1837,10 +1837,10 @@ data:extend({
     type = "fluid",
     name = "pm-2-phenylpyridine",
     icon_size = 64,
-    icon = "__periodic-madness__/graphics/icons/placeholder-item.png",
+    icon = "__periodic-madness__/graphics/icons/fluids/2-phenylpyridine.png",
     default_temperature = 15,
-    base_color = {r = 0.3, g = 0.3, b = 0.2},
-    flow_color = {r = 0.3, g = 0.3, b = 0.2},
+    base_color = {r = 0.25, g = 0.25, b = 0.4},
+    flow_color = {r = 0.25, g = 0.25, b = 0.4},
   },
   {
     type = "fluid",
