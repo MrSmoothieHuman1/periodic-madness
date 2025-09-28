0b00dccc77ce6d19f86e399282fc997dea51f762
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-09-28 11:09:32 +0100

    added tech icon for advanced coke processing 2

--- prototype/technology/technologies.lua
+++ prototype/technology/technologies.lua
@@ -2511,7 +2511,7 @@ data:extend({
     type = "technology",
     name = "pm-advanced-coke-processing-2",
     icon_size = 256,
-    icon = "__periodic-madness__/graphics/technology/advanced-coke-processing.png",
+    icon = "__periodic-madness__/graphics/technology/advanced-coke-processing-2.png",
     effects =
     {
       PM.unlock_recipe("pm-stone-coke-oven-3"),
