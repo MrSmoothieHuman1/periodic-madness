e79b4233fb6d5dd9685d743e5d183871a2e32864
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-09-29 14:57:35 +0100

    added selenium bots

--- prototype/robots.lua
+++ prototype/robots.lua
@@ -374,6 +374,110 @@ local robots =
     shadow_idle_with_cargo = robot_animations.pm_lithium_logistic_robot.shadow_idle_with_cargo,
     shadow_in_motion = robot_animations.pm_lithium_logistic_robot.shadow_in_motion,
     shadow_in_motion_with_cargo = robot_animations.pm_lithium_logistic_robot.shadow_in_motion_with_cargo
+  },
+  {
+    type = "construction-robot",
+    name = "pm-selenium-construction-robot",
+    icon = "__periodic-madness__/graphics/icons/lithium-construction-robot.png",
+    icon_size = 64,
+    flags = { "placeable-player", "player-creation", "placeable-off-grid", "not-on-map" },
+    minable = { mining_time = 0.1, result = "pm-lithium-construction-robot" },
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 64
+      }
+    },
+    max_health = 200,
+    collision_box = { { 0, 0 }, { 0, 0 } },
+    selection_box = { { -0.5, -1.5 }, { 0.5, -0.5 } },
+    hit_visualization_box = { { -0.1, -1.1 }, { 0.1, -1.0 } },
+    damaged_trigger_effect = hit_effects.flying_robot(),
+    dying_explosion = "construction-robot-explosion",
+    corspe = "pm-lithium-construction-robot-remnants",
+    max_payload_size = 2,
+    speed = 0.08,
+    max_energy = "5MJ",
+    energy_per_tick = "0.05kJ",
+    speed_multiplier_when_out_of_energy = 0.2,
+    energy_per_move = "2.5kJ",
+    min_to_charge = 0.25,
+    max_to_charge = 1,
+    icon_draw_specification = {scale = 0.5},
+    --working_light = {intensity = 0.8, size = 3, color = {r = 0.8, g = 0.8, b = 0.8}},
+    smoke =
+    {
+      filename = "__base__/graphics/entity/smoke-construction/smoke-01.png",
+      width = 39,
+      height = 32,
+      frame_count = 19,
+      line_length = 19,
+      shift = { 0.078125, -0.15625 },
+      animation_speed = 0.3
+    },
+    sparks = robot_animations.sparks,
+    repairing_sound =
+    {
+      { filename = "__base__/sound/robot-repair-1.ogg", volume = 0.6 },
+      { filename = "__base__/sound/robot-repair-2.ogg", volume = 0.6 },
+      { filename = "__base__/sound/robot-repair-3.ogg", volume = 0.6 },
+      { filename = "__base__/sound/robot-repair-4.ogg", volume = 0.6 },
+      { filename = "__base__/sound/robot-repair-5.ogg", volume = 0.6 },
+      { filename = "__base__/sound/robot-repair-6.ogg", volume = 0.6 },
+    },
+    working_sound = sounds.construction_robot(0.47),
+    cargo_centered = { 0.0, 0.2 },
+    construction_vector = { 0.30, 0.22 },
+    water_reflection = robot_reflection(1),
+    idle = robot_animations.pm_lithium_construction_robot.idle,
+    in_motion = robot_animations.pm_lithium_construction_robot.in_motion,
+    shadow_idle = robot_animations.pm_lithium_construction_robot.shadow_idle,
+    shadow_in_motion = robot_animations.pm_lithium_construction_robot.shadow_in_motion,
+    working = robot_animations.pm_lithium_construction_robot.working,
+    shadow_working = robot_animations.pm_lithium_construction_robot.shadow_working
+  },
+  {
+    type = "logistic-robot",
+    name = "pm-selenium-logistic-robot",
+    icon = "__periodic-madness__/graphics/icons/lithium-logistic-robot.png",
+    icon_size = 64,
+    flags = { "placeable-player", "player-creation", "placeable-off-grid", "not-on-map" },
+    minable = { mining_time = 0.1, result = "pm-lithium-logistic-robot" },
+    icon_draw_specification = {scale = 0.5},
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 64
+      }
+    },
+    max_health = 200,
+    collision_box = { { 0, 0 }, { 0, 0 } },
+    selection_box = { { -0.5, -1.5 }, { 0.5, -0.5 } },
+    hit_visualization_box = { { -0.1, -1.1 }, { 0.1, -1.0 } },
+    damaged_trigger_effect = hit_effects.flying_robot(),
+    dying_explosion = "logistic-robot-explosion",
+    corspe = "pm-lithium-logistic-robot-remnants",
+    max_payload_size = 2,
+    speed = 0.08,
+    max_energy = "5MJ",
+    energy_per_tick = "0.05kJ",
+    speed_multiplier_when_out_of_energy = 0.2,
+    energy_per_move = "2.5kJ",
+    min_to_charge = 0.25,
+    max_to_charge = 1,
+    working_sound = sounds.flying_robot(0.48),
+    cargo_centered = { 0.0, 0.2 },
+    water_reflection = robot_reflection(1),
+    idle = robot_animations.pm_lithium_logistic_robot.idle,
+    idle_with_cargo = robot_animations.pm_lithium_logistic_robot.idle_with_cargo,
+    in_motion = robot_animations.pm_lithium_logistic_robot.in_motion,
+    in_motion_with_cargo = robot_animations.pm_lithium_logistic_robot.in_motion_with_cargo,
+    shadow_idle = robot_animations.pm_lithium_logistic_robot.shadow_idle,
+    shadow_idle_with_cargo = robot_animations.pm_lithium_logistic_robot.shadow_idle_with_cargo,
+    shadow_in_motion = robot_animations.pm_lithium_logistic_robot.shadow_in_motion,
+    shadow_in_motion_with_cargo = robot_animations.pm_lithium_logistic_robot.shadow_in_motion_with_cargo
   }
 }
 
