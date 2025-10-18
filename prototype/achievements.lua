427db942e705aff5b1c4ed7fc1bac5551c4bb581
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-10-18 16:50:09 +0100

    added nuclear power? no thanks achievement

--- prototype/achievements.lua
+++ prototype/achievements.lua
@@ -15,7 +15,16 @@ data:extend({
     icon = "__periodic-madness__/graphics/achievements/legacy-code.png",
     dont_research = {"processing-unit"},
     research_with = {"pm-noble-gas-science-pack"}
-  }
+  },
+{
+    type = "dont-use-entity-in-energy-production-achievement",
+    name = "pm-nuclear-power-no-thanks",
+    icon_size = 128,
+    icon = "__periodic-madness__/graphics/achievements/legacy-code.png",
+    excluded = {"steam-engine", "steam-turbine", "solar-panel", "pm-solar-panel-2", "pm-solar-panel-3", "pm-hydroelectric-plant"},
+    included = {"nuclear-reactor", "pm-polonium-reactor", "pm-RTG"},
+    objective_condition = "rocket-launched",
+  },
 })
 
 data.raw["complete-objective-achievement"]["there-is-no-spoon"].within = 100 * hour
