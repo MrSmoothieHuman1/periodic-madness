19800557d914a4fbb30ecd8ba51bc614f7906c82
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-10-30 17:22:24 +0000

    added graphene icon

--- prototype/item.lua
+++ prototype/item.lua
@@ -1762,7 +1762,7 @@ data:extend({
     type = "item",
     name = "pm-graphene",
     icon_size = 64,
-    icon = "__periodic-madness__/graphics/icons/YAG-laser.png",
+    icon = "__periodic-madness__/graphics/icons/graphene.png",
     subgroup = "pm-garnet-ptm",
     order = "d",
     stack_size = 250
