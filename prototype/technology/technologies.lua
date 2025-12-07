f9c2f1941632c9eafd3da4b6a7083fcaaccea6ae
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-12-07 18:38:05 +0000

    added probably-needs changing hafnium tech icon

--- prototype/technology/technologies.lua
+++ prototype/technology/technologies.lua
@@ -6177,7 +6177,7 @@ data:extend({
     type = "technology",
     name = "pm-hafnium-processing",
     icon_size = 256,
-    icon = "__periodic-madness__/graphics/icons/placeholder-tech.png",
+    icon = "__periodic-madness__/graphics/technology/hafnium-processing.png",
     effects =
     {
         PM.unlock_recipe("pm-hafnium-chunks"),
