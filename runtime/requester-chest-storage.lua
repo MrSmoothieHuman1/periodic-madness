22962b4c1645d2bdca62612801b2821241c67763
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-12-14 01:36:21 +0000

    updated requester rework to be... functional

--- runtime/requester-chest-storage.lua
+++ runtime/requester-chest-storage.lua
@@ -2,7 +2,8 @@
 local handler = {events = {}}
 
 local function setup_logistic_storage()
-    storage.logistic_chest_override = storage.logistic_chest_override + count
+    storage.logistic_chest_override = 1
+    game.print("setup")
 
     if not storage.logistic_chests then
         local chests = {}
@@ -24,12 +25,35 @@ PM.compound_events.built_events(handler.events, function (event)
     local tracked_entity = event.entity or event.destination
 
     if tracked_entity.name ~= "requester-chest" then return end
+
+    storage.logistic_chests[tracked_entity.unit_number] = tracked_entity
+    tracked_entity.set_inventory_size_override(defines.inventory.chest, storage.logistic_chest_override)
+
 end)
 
 handler.events[defines.events.on_research_finished] = function (event)
   if event.research.name == "pm-osmium-processing" then
-        for _, entity in pairs(storage.logistic_chests) do
-            entity.set_inventory_size_override(defines.inventory.chest, 2)
+        storage.logistic_chest_override = storage.logistic_chest_override + 1
+        game.print("researched")
+        for unit_number, entity in pairs(storage.logistic_chests) do
+            if entity.valid then
+                entity.set_inventory_size_override(defines.inventory.chest, storage.logistic_chest_override)
+            else
+                storage.logistic_chests[unit_number] = nil
+            end
+        end
+    end
+end
+handler.events[defines.events.on_research_reversed] = function (event)
+  if event.research.name == "pm-osmium-processing" then
+        game.print("unresearched :(")
+        storage.logistic_chest_override = storage.logistic_chest_override - 1
+        for unit_number, entity in pairs(storage.logistic_chests) do
+            if entity.valid then
+                entity.set_inventory_size_override(defines.inventory.chest, storage.logistic_chest_override)
+            else
+                storage.logistic_chests[unit_number] = nil
+            end
         end
     end
 end
