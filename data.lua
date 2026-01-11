c1df3b63ef03c2861e23f645775ee0c89fb67d7c
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2026-01-11 12:43:49 +0000

    removed pumpjack mining radius

--- data.lua
+++ data.lua
@@ -830,5 +830,3 @@ local layers = set.working_visualisations[2].animation.layers
 layers[1].filename = "__periodic-madness__/graphics/entities/buildings/centrifuge/centrifuge-C-light.png"
 layers[2].filename = "__periodic-madness__/graphics/entities/buildings/centrifuge/centrifuge-B-light.png"
 layers[3].filename = "__periodic-madness__/graphics/entities/buildings/centrifuge/centrifuge-A-light.png"
-
-data.raw["mining-drill"]["pumpjack"].resource_searching_radius = 4.99
