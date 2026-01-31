e3f60358eca64679f8c44b39a86b126eea94d2df
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2026-01-31 18:50:31 +0000

    added cerium icon and variations

--- prototype/elements/058-cerium.lua
+++ prototype/elements/058-cerium.lua
@@ -31,12 +31,14 @@ data:extend({
     {
       type = "item",
       name = "pm-cerium",
-      icon_size = 128,
-      icon = "__periodic-madness__/graphics/icons/ores/bastnasite-Y.png",
+      icon_size = 64,
+      icon = "__periodic-madness__/graphics/icons/ores/cerium.png",
       pictures =
       {
-        { filename = "__periodic-madness__/graphics/icons/ores/bastnasite-Y.png", size = 128, scale = 0.25},
-        { filename = "__periodic-madness__/graphics/icons/ores/bastnasite-Y-2.png", size = 128, scale = 0.25},
+        { filename = "__periodic-madness__/graphics/icons/ores/cerium.png", size = 64, scale = 0.5},
+        { filename = "__periodic-madness__/graphics/icons/ores/cerium-2.png", size = 64, scale = 0.5},
+        { filename = "__periodic-madness__/graphics/icons/ores/cerium-3.png", size = 64, scale = 0.5},
+        { filename = "__periodic-madness__/graphics/icons/ores/cerium-4.png", size = 64, scale = 0.5},
       },
       subgroup = "pm-cerium-la",
       order = "c",
