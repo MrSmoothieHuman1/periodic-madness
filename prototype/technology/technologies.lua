48e8d385f979c08a6b11bc881fb1690394961128
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2026-08-30 19:03:27 +0100

    added advanced science production tech icon

--- prototype/technology/technologies.lua
+++ prototype/technology/technologies.lua
@@ -7081,7 +7081,7 @@ data:extend({
     type = "technology",
     name = "pm-advanced-science-production",
     icon_size = 256,
-    icon = "__periodic-madness__/graphics/icons/placeholder-tech.png",
+    icon = "__periodic-madness__/graphics/technology/advanced-science-production.png",
     effects =
     {
         PM.unlock_recipe("pm-liquid-transition-metal-science"),
