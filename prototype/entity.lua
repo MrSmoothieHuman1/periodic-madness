b99be4dd42c1b56925863019501a45895e36ffe5
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2026-09-11 20:03:21 +0100

    added superconductive substation entity

--- prototype/entity.lua
+++ prototype/entity.lua
@@ -12272,6 +12272,154 @@ fluid_boxes =
   },
 },
 }--[[@as data.AssemblingMachinePrototype]],
+  {
+    type = "electric-pole",
+    name = "pm-superconductive-substation",
+    icon = "__base__/graphics/icons/substation.png",
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {mining_time = 0.2, result = "substation"},
+    fast_replaceable_group = "substation",
+    max_health = 800,
+    corpse = "substation-remnants",
+    dying_explosion = "substation-explosion",
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 90
+      }
+    },
+    collision_box = {{-0.7, -0.7}, {0.7, 0.7}},
+    selection_box = {{-1, -1}, {1, 1}},
+    damaged_trigger_effect = hit_effects.entity({{-0.5, -2.5}, {0.5, 0.5}}),
+    drawing_box_vertical_extension = 2,
+    maximum_wire_distance = 32,
+    supply_area_distance = 16,
+    pictures =
+    {
+      layers =
+      {
+
+        {
+          filename = "__base__/graphics/entity/substation/substation.png",
+          priority = "high",
+          width = 138,
+          height = 270,
+          direction_count = 4,
+          shift = util.by_pixel(0, 1-32),
+          scale = 0.5
+        },
+        {
+          filename = "__base__/graphics/entity/substation/substation-shadow.png",
+          priority = "high",
+          width = 370,
+          height = 104,
+          direction_count = 4,
+          shift = util.by_pixel(62, 42-32),
+          draw_as_shadow = true,
+          scale = 0.5
+        }
+      }
+    },
+
+    impact_category = "metal",
+    open_sound = sounds.electric_network_open,
+    close_sound = sounds.electric_network_close,
+    working_sound =
+    {
+      sound =
+      {
+        filename = "__base__/sound/substation.ogg",
+        volume = 0.4,
+        audible_distance_modifier = 0.32,
+      },
+      max_sounds_per_prototype = 3,
+      fade_in_ticks = 30,
+      fade_out_ticks = 40,
+      use_doppler_shift = false
+    },
+    connection_points =
+    {
+      {
+        shadow =
+        {
+          copper = util.by_pixel(136, 8),
+          green = util.by_pixel(124, 8),
+          red = util.by_pixel(151, 9)
+        },
+        wire =
+        {
+          copper = util.by_pixel(0, -86),
+          green = util.by_pixel(-21, -82),
+          red = util.by_pixel(22, -81)
+        }
+      },
+      {
+        shadow =
+        {
+          copper = util.by_pixel(133, 9),
+          red = util.by_pixel(144, 21),
+          green = util.by_pixel(110, -3)
+        },
+        wire =
+        {
+          copper = util.by_pixel(0, -85),
+          red = util.by_pixel(15, -70),
+          green = util.by_pixel(-15, -92)
+        }
+      },
+      {
+        shadow =
+        {
+          copper = util.by_pixel(133, 9),
+          red = util.by_pixel(127, 26),
+          green = util.by_pixel(127, -8)
+        },
+        wire =
+        {
+          copper = util.by_pixel(0, -85),
+          red = util.by_pixel(0, -66),
+          green = util.by_pixel(0, -97)
+        }
+      },
+      {
+        shadow =
+        {
+          copper = util.by_pixel(133, 9),
+          red = util.by_pixel(111, 20),
+          green = util.by_pixel(144, -3)
+        },
+        wire =
+        {
+          copper = util.by_pixel(0, -86),
+          red = util.by_pixel(-15, -71),
+          green = util.by_pixel(15, -92)
+        }
+      }
+    },
+    radius_visualisation_picture =
+    {
+      filename = "__base__/graphics/entity/small-electric-pole/electric-pole-radius-visualization.png",
+      width = 12,
+      height = 12,
+      priority = "extra-high-no-scale"
+    },
+    water_reflection =
+    {
+      pictures =
+      {
+        filename = "__base__/graphics/entity/substation/substation-reflection.png",
+        priority = "extra-high",
+        width = 20,
+        height = 28,
+        shift = util.by_pixel(0, 55),
+        variation_count = 1,
+        scale = 5
+      },
+      rotate = false,
+      orientation_to_variation = false
+    }
+  },
 })
 data:extend({
 fireutil.add_magnesium_fire_graphics_and_effects_definitions
